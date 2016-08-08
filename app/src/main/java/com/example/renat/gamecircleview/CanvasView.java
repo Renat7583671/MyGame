c11b441298210e20ff2c34894d35c041cdd62048
renat <cherenat777@gmail.com>
2016-08-08 19:43:20 +0300

    first commit

---
+++ app/src/main/java/com/example/renat/gamecircleview/CanvasView.java
@@ -0,0 +1,14 @@
+package com.example.renat.gamecircleview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by renat on 08.08.16.
+ */
+public class CanvasView extends View {
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/renat/gamecircleview/SimplelCircle.java
@@ -0,0 +1,7 @@
+package com.example.renat.gamecircleview;
+
+/**
+ * Created by renat on 08.08.16.
+ */
+public class SimplelCircle {
+}
